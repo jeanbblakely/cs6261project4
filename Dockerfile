12292b6782823e676c474198a0d1c348ee3bd345
Jeannie Blakely <jeanbblakely@gmail.com>
2019-07-23 17:14:50 -0400

    created models; bound to html

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL "maintainer"="jeanbblakely@gmail.com"
 WORKDIR /work/cs6261project4
 
 RUN npm i npm@latest -g
-RUN npm install -g @angular/cli@7.3.9
+RUN npm install -g @angular/cli@7.3.5
 RUN npm install http-server -g
 
 CMD ["/work/cs6261project4/script.sh"]
--- src/app/calculator/calculator/calculator.component.ts
+++ src/app/calculator/calculator/calculator.component.ts
@@ -10,24 +10,51 @@ export class CalculatorComponent implements OnInit {
   public digit1; 
   public digit2;
   public digit3;
-  public colors = [
-    'Black',
-    'Brown',
-    'Red',
-    'Orange',
-    'Yellow',
-    'Green',
-    'Blue',
-    'Violet',
-    'Grey',
-    'White',
-    'Gold',
-    'Silver'
+  public multiplier;
+  public tolerance;
+  public digits = [
+    { color: 'Black', value: 0 },
+    { color: 'Brown', value: 1 },
+    { color: 'Red', value: 2 },
+    { color: 'Orange', value: 3 },
+    { color: 'Yellow', value: 4 },
+    { color: 'Green', value: 5 },
+    { color: 'Blue', value: 6 },
+    { color: 'Violet', value: 7 },
+    { color: 'Grey', value: 8 },
+    { color: 'White', value: 9 }
+  ];
+  public multipliers = [
+    { color: 'Black', value: 1 },
+    { color: 'Brown', value: 10 },
+    { color: 'Red', value: 100 },
+    { color: 'Orange', value: 1000 },
+    { color: 'Yellow', value: 10000 },
+    { color: 'Green', value: 100000 },
+    { color: 'Blue', value: 100000 },
+    { color: 'Violet', value: 100000 },
+    { color: 'Grey', value: 1000000 },
+    { color: 'White', value: 10000 },
+    { color: 'Gold', value: 10 },
+    { color: 'Silver', value: 100 }
+  ];
+  public tolerances = [
+    { color: 'Brown', value: 1 },
+    { color: 'Red', value: 2 },
+    { color: 'Orange', value: 3 },
+    { color: 'Yellow', value: 4 },
+    { color: 'Green', value: 0.5 },
+    { color: 'Blue', value:  0.25 },
+    { color: 'Violet', value: 0.10 },
+    { color: 'Grey', value: 0.05 },
+    { color: 'Gold', value: 5 },
+    { color: 'Silver', value: 10 }
   ];
     
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
