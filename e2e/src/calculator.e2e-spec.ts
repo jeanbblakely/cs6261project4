2ac1b026402411182aa32d2b7d0cd5a69d561a7f
Jeannie Blakely <jeanbblakely@gmail.com>
2019-07-25 07:25:34 -0400

    dropdown select text asserting in e2e

--- e2e/src/calculator.e2e-spec.ts
+++ e2e/src/calculator.e2e-spec.ts
@@ -10,6 +10,17 @@ describe('Resistance Calculator', () => {
     browser.get('/');
     let text = element(by.id('resistance')).getText();
     expect(text).toEqual('Resistance: 0 ohms +/-1%');
+    text = element(by.id('digit-1')).element(by.css('option:checked')).getText();
+    expect(text).toEqual('Black');
+    text = element(by.id('digit-2')).element(by.css('option:checked')).getText();
+    expect(text).toEqual('Black');
+    text = element(by.id('digit-3')).element(by.css('option:checked')).getText();
+    expect(text).toEqual('Black');
+    
+    text = element(by.id('multiplier')).element(by.css('option:checked')).getText();
+    expect(text).toEqual('Black');
+    text = element(by.id('tolerance')).element(by.css('option:checked')).getText();
+    expect(text).toEqual('Brown');
 
   }); 
 });
